d235a8264a392573dc5bae110c5e033391720ab4
Andreas <westplay22@gmail.com>
2018-03-18 22:01:12 +0000

    SortAlphabeticallyAscending test

---
+++ app/src/test/java/movielister/andreas/com/movielister/listmovies/domain/SortAlphabeticallyAscendingTest.java
@@ -0,0 +1,38 @@
+package movielister.andreas.com.movielister.listmovies.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import io.reactivex.observers.TestObserver;
+
+public class SortAlphabeticallyAscendingTest {
+
+    private SortAlphabeticallyAscending sortAlphabeticallyAscending;
+
+    @Before
+    public void setUp() {
+        sortAlphabeticallyAscending = new SortAlphabeticallyAscending();
+    }
+
+    @Test
+    public void execute() throws Exception {
+        List<Movie> movieList = Arrays.asList(createMovie("The Matrix"), createMovie("Animaniacs"));
+        List<Movie> expectedList = Arrays.asList(createMovie("Animaniacs"), createMovie("The Matrix"));
+
+        TestObserver<List<Movie>> test = sortAlphabeticallyAscending.execute(movieList).test();
+
+        test.assertValue(expectedList)
+                .assertNoErrors();
+    }
+
+    private Movie createMovie(String title) {
+        return Movie.builder()
+                .title(title)
+                .movieImage("image.png")
+                .movieGenre("Sci-Fi")
+                .build();
+    }
+}
