ae09b7ae05a0086514d66099fd302d6b241ed83b
Andreas <westplay22@gmail.com>
2018-03-18 18:13:03 +0000

    Deleting the cached movies when  updating the cache

--- app/src/main/java/movielister/andreas/com/movielister/listmovies/data/cache/CachedMoviesDao.java
+++ app/src/main/java/movielister/andreas/com/movielister/listmovies/data/cache/CachedMoviesDao.java
@@ -4,18 +4,28 @@ import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Transaction;
 
 import java.util.List;
 
 import io.reactivex.Flowable;
 
 @Dao
-public interface CachedMoviesDao {
+public abstract class CachedMoviesDao {
+
+    @Transaction
+    public void updateCache(List<CachedMovie> movies) {
+        deleteAllMovies();
+        insertMovies(movies);
+    }
 
     @Query("select * from CachedMovie")
-    Flowable<List<CachedMovie>> getCachedMovies();
+    public abstract Flowable<List<CachedMovie>> getCachedMovies();
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void updateCache(List<CachedMovie> movies);
+    public abstract void insertMovies(List<CachedMovie> movies);
+
+    @Query("DELETE FROM CachedMovie")
+    public abstract void deleteAllMovies();
 
 }
